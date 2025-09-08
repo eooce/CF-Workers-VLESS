15eba28017f91944a99bdf5812702fadcbf3c5e4
老王 <sun@f4i.cn>
2025-09-09 01:19:33 +0800

    Init commit

--- _worker.js
+++ _worker.js
@@ -5,10 +5,10 @@
 
 import { connect } from 'cloudflare:sockets';
 
-let subPath = 'link';     // 订阅路径
-let password = '123456';  // 主页密码
+let subPath = 'link';     // 订阅路径,默认将使用uuid作为订阅路径
+let password = '123456';  // 主页密码，建议修改或添加PASSWORD环境变量
 let serverPool = ['13.230.34.30'];  // proxyIP
-let yourUUID = '5dc15e15-f285-4a9d-959b-0e4fbdd77b63'; // UUID
+let yourUUID = '5dc15e15-f285-4a9d-959b-0e4fbdd77b63'; // UUID，建议修改或添加环境便量
 
 let cfip = [ // cfip
 	'ip.sb', 'time.is', 'skk.moe', 'www.visa.com.tw', 'www.visa.com.hk', 'www.visa.com.sg',
